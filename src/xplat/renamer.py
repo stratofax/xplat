2b5d68df32c28798d978d35858b632851565b5ed
Neil Johnson <neil@cadent.com>
2023-02-26 17:53:52 -0500

    Strict renaming

--- src/xplat/renamer.py
+++ src/xplat/renamer.py
@@ -1,20 +1,44 @@
 """
-Changes filename to avoid platform, path issues:
+Changes filename to avoid platform, path, URL issues:
 * Spaces to delimiter
 * Dots to underscore
 * All lowercase
 * web and internet friendly
-
+Characters allowed in a URL:
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+abcdefghijklmnopqrstuvwxyz
+0123456789
+Special characters:
+safe  $-_.+
+reserved  ;/?:@&=
+extra  ,[] 
+https://www.rfc-editor.org/rfc/rfc3986
+or
+https://www.ietf.org/rfc/rfc1738.txt
 """
 
 from pathlib import Path
 
 
-def inet_names(
+def safe_stem(orig_stem: str, delim: str = "_") -> str:
+    """
+    Change spaces to delimiter (default = _, or -)
+    Change dots to underscore
+    Change all to lowercase
+    Remove all other special characters
+    Replace double delimiters with single
+    """
+    new_stem = orig_stem.replace(" ", delim).replace(".", "_").lower()
+    # remove all special characters
+    new_stem = "".join([c for c in new_stem if c.isalnum() or c == delim])
+    # replace double delimiters with single
+    return new_stem.replace(delim + delim, delim)
+
+
+def safe_renamer(
     abs_path: Path,
     target_dir: Path = None,
     delim_chr: str = "_",
-    dots: str = "_",
     dry_run: bool = False,
 ) -> str:
     """
@@ -26,22 +50,20 @@ def inet_names(
     if target dir exists, move renamed file to it
     """
     if not abs_path.is_file():
-        return f"ERROR: {abs_path} is not a file."
+        raise FileNotFoundError(f"{abs_path} is not a file.")
     if target_dir is not None and not target_dir.is_dir():
-        return f"ERROR: {target_dir} is not a directory."
-
+        raise NotADirectoryError(f"{target_dir} is not a directory.")
     # replace periods in file stem with dots delimiter
-    new_stem = abs_path.stem.replace(".", dots).lower()
-    # update stem: replace spaces with specified delimiter
-    new_stem = new_stem.replace(" ", delim_chr)
+    new_stem = safe_stem(abs_path.stem, delim_chr)
     # merge the stem with the lowercase suffix
     new_suffix = abs_path.suffix.lower()
-    new_name = f"{new_stem}{new_suffix}"
-    # rename file in existing dir if no target provided
+    new_filename = f"{new_stem}{new_suffix}"
+    # copy file to target dir if provided
     if target_dir is not None:
-        new_path = target_dir.joinpath(new_name)
+        new_path = target_dir.joinpath(new_filename)
+    # rename file in existing dir if no target provided
     else:
-        new_path = abs_path.with_name(new_name)
+        new_path = abs_path.with_name(new_filename)
     if not dry_run:
         abs_path.rename(new_path)
     return new_path
