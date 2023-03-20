51562f323aa18c1657e1cb5ef28e28e113422389
Neil Johnson <neil@cadent.com>
2023-03-19 20:42:35 -0400

    Debug test_cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,8 +8,12 @@ from xplat.cli import app
 # create test files & dirs
 _test_path = Path.home().joinpath("tmp", "xplat_cli_tests")
 _test_path.mkdir(parents=True, exist_ok=True)
-_list_file = _test_path / "list_file_test.txt"
-_list_file.touch(exist_ok=True)
+_list_file_1 = _test_path / "list_file_1.txt"
+_list_file_1.touch(exist_ok=True)
+_list_file_2 = _test_path / "list_file_2.txt"
+_list_file_2.touch(exist_ok=True)
+_list_file_3 = _test_path / "list_file_3.txt"
+_list_file_3.touch(exist_ok=True)
 
 runner = CliRunner()
 
@@ -37,9 +41,13 @@ def test_list():
     result = runner.invoke(app, "list")
     assert result.exit_code == 0
     # list files in test directory
-    result = runner.invoke(app, "list", str(_test_path))
+    result = runner.invoke(app, ["list", _test_path])
     assert result.exit_code == 0
-    result = runner.invoke(app, "list", str(_list_file))
+    assert _list_file_1.name in result.stdout
+    assert "Total files found = 3" in result.stdout
+    result = runner.invoke(app, ["list", str(_test_path), "--ext", "txt"])
+    assert result.exit_code == 0
+    result = runner.invoke(app, "list", str(_list_file_1))
     assert result.exit_code == 0
 
 
