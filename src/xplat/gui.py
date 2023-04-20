ad784f638346a5ce9b80059892c976c9c3ae7036
Neil Johnson <neil@cadent.com>
2023-04-20 14:41:15 -0400

    Use ttk

--- src/xplat/gui.py
+++ src/xplat/gui.py
@@ -1,5 +1,6 @@
 """The GUI module for the xplat package."""
 import tkinter as tk
+import tkinter.ttk as ttk
 
 from xplat import constants
 
@@ -10,44 +11,34 @@ FONT = 'Arial'
 BACKGROUND = 'white'
 FOREGROUND = 'black'
 
-
-class XplatStart(tk.Tk):
-    """The main application class for the xplat package."""
-    def __init__(self):
-        """Initialize the application."""
-        WIDTH = 400
-        HEIGHT = 300
-        super().__init__()
-        self.title(PROGRAM_NAME)
-        self.geometry(f"{WIDTH}x{HEIGHT}")
-        self.configure(bg=BACKGROUND)
-
-        program_name = tk.Label(self, text=PROGRAM_NAME,
-                                font=(FONT, 20, 'bold'),
-                                bg=BACKGROUND, fg=FOREGROUND)
-        program_name.pack(pady=20)
-
-        # Create a Label widget with center-aligned text
-        app_help = tk.Label(self, text=APP_HELP,
-                            font=(FONT, 12, 'italic'),
-                            bg=BACKGROUND, fg=FOREGROUND,
-                            width=40, height=3, wraplength=WIDTH*0.8,
-                            justify='center')
-        app_help.pack()
-
-        version = f"Version {VERSION}\n"
-        # Create a Text widget and add some initial text
-        app_version = tk.Label(self, text=version,
-                               font=(FONT, 12), bg=BACKGROUND, fg=FOREGROUND,
-                               width=40, height=3, wraplength=WIDTH*0.8,
-                               justify='center')
-        app_version.pack()
+WIDTH = 400
+HEIGHT = 300
 
 
 def start_gui() -> None:
     """Start the GUI."""
-    app = XplatStart()
-    app.mainloop()
+    window = tk.Tk()
+    window.title(PROGRAM_NAME)
+    window.geometry(f"{WIDTH}x{HEIGHT}")
+
+    program_name = ttk.Label(window, text=PROGRAM_NAME,
+                             font=(FONT, 20, 'bold'))
+    program_name.pack(pady=20)
+
+# Create a Label widget with center-aligned text
+    app_help = ttk.Label(window, text=APP_HELP,
+                         font=(FONT, 12, 'italic'),
+                         )
+    app_help.pack(padx=40)
+
+    version = f"Version {VERSION}\n"
+# Create a Text widget and add some initial text
+    app_version = ttk.Label(window, text=version,
+                            font=(FONT, 12),
+                            justify='center')
+    app_version.pack()
+
+    window.mainloop()
 
 
 if __name__ == "__main__":
