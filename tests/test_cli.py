debbb96113546f6a04240102fe85836c2b5f761f
Neil Johnson <neil@cadent.com>
2023-04-29 16:47:20 -0400

    Increase test coverarge

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from typer.testing import CliRunner
 
 from xplat import constants
-from xplat.cli import app
+from xplat.cli import app, check_dir, print_files
 
 _runner = CliRunner()
 
@@ -39,26 +39,36 @@ def test_list():
     list_file_3 = test_path.joinpath("list_file_3.txt")
     list_file_3.touch(exist_ok=True)
     # check for required argument
-    test_required_arg = _runner.invoke(app, "list")
+    test_required_arg = _runner.invoke(app, "list", input="q\n")
     assert test_required_arg.exit_code == 0
     assert "Total files found =" in test_required_arg.stdout
     # list files in test directory.
-    test_list_dir = _runner.invoke(app, ["list", str(test_path)])
+    test_list_dir = _runner.invoke(app, ["list", str(test_path)], input="q\n")
     assert test_list_dir.exit_code == 0
     assert list_file_3.name in test_list_dir.stdout
     assert "Total files found = 3" in test_list_dir.stdout
     # list files in test directory with 'txt' extension
-    test_ext = _runner.invoke(app, ["list", str(test_path), "--ext", "txt"])
+    test_ext = _runner.invoke(app, ["list", str(test_path), "--ext", "txt"],
+                              input="q\n")
     assert test_ext.exit_code == 0
     assert list_file_2.name in test_ext.stdout
     assert "Total files found = 3" in test_ext.stdout
     # list info for a single file
-    test_file = _runner.invoke(app, ["list", str(list_file_1)])
+    test_file = _runner.invoke(app, ["list", str(list_file_1)], input="q\n")
     assert test_file.exit_code == 0
     assert list_file_1.name in test_file.stdout
-    assert "Size: 20.0 K" in test_file.stdout
+    assert "Size:     20.0 K" in test_file.stdout
     # remove test files from test dir
     for file in test_path.iterdir():
         file.unlink()
     # remove test dir
     test_path.rmdir()
+    # check for non-existent directory
+    assert check_dir(test_path, "Test") is False
+
+
+def test_print_files():
+    # test for empty list
+    test_empty = []
+    list_count = print_files(test_empty)
+    assert list_count == 0
